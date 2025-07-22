58fcc4c2edaa265ee198f5dd3434f89c3c9b0422
gabrielcn07 <gabrielchagasn@gmail.com>
2025-07-22 17:11:42 +0000

    Licao 1 - Excecoes

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,12 @@
+public class Numero {
+    public static void dividir(int a, int b){
+        int resultado = 0;
+        try{
+            resultado = a / b;
+        }catch(Exception e){
+            System.out.printf("Nao eh possivel dividir por zero\n");
+        }finally{
+            System.out.printf("%d / %d = %d\n", a, b, resultado);
+        }
+    }
+}
