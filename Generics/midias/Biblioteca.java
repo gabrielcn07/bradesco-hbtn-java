cf3dcfa366d039df98cd5b3b4f26c2cfbe0768bf
gabrielcn07 <gabrielchagasn@gmail.com>
2025-07-24 17:28:06 +0000

    Generics

---
+++ Generics/midias/Biblioteca.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Biblioteca<T extends Midia> {
+    List<T> midias = new ArrayList<>();
+
+    public void adicionarMidia(T midia){
+        midias.add(midia);
+    }
+
+    public List<T> obterListaMidias(){
+        return midias;
+    }
+}
---
+++ Generics/midias/Midia.java
@@ -0,0 +1,12 @@
+public class Midia {
+    private String nome;
+
+    public Midia(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString (){
+       return String.format("Tipo: %s - Nome: %s", this.getClass().getSimpleName(), nome); 
+    }
+}
