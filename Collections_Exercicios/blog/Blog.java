de3dc43295ecbe685f5a45146556f0b8a58c5003
gabrielcn07 <gabrielchagasn@gmail.com>
2025-07-28 14:07:12 +0000

    Collections 1

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Blog {
+    List<Post> posts = new ArrayList<Post>();
+
+    public void adicionarPostagem(Post post){
+        posts.add(post);
+    }
+
+    public Set<String> obterTodosAutores(){
+        Set<String> conjuntoOrdenado = new TreeSet<>();
+        for(Post post : posts){
+           conjuntoOrdenado.add(post.getAutor());
+        }
+        return conjuntoOrdenado;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria(){
+        Map<String, Integer> contagem = new TreeMap<>();
+        for (Post post : posts) {
+            String categoria = post.getCategoria();
+            contagem.put(categoria, contagem.getOrDefault(categoria, 0) + 1);
+        }
+        return contagem;
+    }
+}
