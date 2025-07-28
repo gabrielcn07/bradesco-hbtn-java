39afd4fd46ddffe34d9ec3f3f01a67ed1329ddc0
gabrielcn07 <gabrielchagasn@gmail.com>
2025-07-28 14:26:10 +0000

    Colletions 2

---
+++ Collections_Exercicios/postagem_blog/Blog.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Blog {
+    private List<Post> listPost = new ArrayList<>();
+
+    public void adicionarPostagem(Post post) {
+        if (listPost.contains(post)) {
+            throw new IllegalArgumentException("Postagem jah existente");
+        }
+        listPost.add(post);
+    }
+
+    public Set<Autor> obterTodosAutores() {
+        Set<Autor> autores = new TreeSet<>();
+        for (Post post : listPost) {
+            autores.add(post.getAutor());
+        }
+        return autores;
+    }
+
+    public Map<Categorias, Integer> obterContagemPorCategoria() {
+        Map<Categorias, Integer> contagem = new HashMap<>();
+        for (Post post : listPost) {
+            Categorias categoria = post.getCategoria();
+            contagem.put(categoria, contagem.getOrDefault(categoria, 0) + 1);
+        }
+        return contagem;
+    }
+
+    public Set<Post> obterPostsPorAutor(Autor autor) {
+        Set<Post> posts = new TreeSet<>();
+        for (Post post : listPost) {
+            if (post.getAutor().equals(autor)) {
+                posts.add(post);
+            }
+        }
+        return posts;
+    }
+
+    public Set<Post> obterPostsPorCategoria(Categorias categoria) {
+        Set<Post> posts = new TreeSet<>();
+        for (Post post : listPost) {
+            if (post.getCategoria() == categoria) {
+                posts.add(post);
+            }
+        }
+        return posts;
+    }
+
+    public Map<Categorias, Set<Post>> obterTodosPostsPorCategorias() {
+        Map<Categorias, Set<Post>> mapa = new HashMap<>();
+        for (Post post : listPost) {
+            mapa.computeIfAbsent(post.getCategoria(), k -> new TreeSet<>()).add(post);
+        }
+        return mapa;
+    }
+
+    public Map<Autor, Set<Post>> obterTodosPostsPorAutor() {
+        Map<Autor, Set<Post>> mapa = new HashMap<>();
+        for (Post post : listPost) {
+            mapa.computeIfAbsent(post.getAutor(), k -> new TreeSet<>()).add(post);
+        }
+        return mapa;
+    }
+}
---
+++ Collections_Exercicios/postagem_blog/Post.java
@@ -0,0 +1,52 @@
+public class Post implements Comparable<Post> {
+    private Autor autor;
+    private String titulo;
+    private String corpo;
+    private Categorias categoria;
+
+    public Post(Autor autor, String titulo, String corpo, Categorias categoria) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.corpo = corpo;
+        this.categoria = categoria;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getCorpo() {
+        return corpo;
+    }
+
+    public Categorias getCategoria() {
+        return categoria;
+    }
+
+    @Override
+    public String toString() {
+        return titulo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Post)) return false;
+        Post post = (Post) o;
+        return autor.equals(post.autor) && titulo.equals(post.titulo);
+    }
+
+    @Override
+    public int hashCode() {
+        return autor.hashCode() + titulo.hashCode();
+    }
+
+    @Override
+    public int compareTo(Post outro) {
+        return this.titulo.compareTo(outro.titulo);
+    }
+}
