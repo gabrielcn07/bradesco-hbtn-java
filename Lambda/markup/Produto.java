ffe133b3413c819cfd56bd04f7ff7bfa3f3a31f3
gabrielcn07 <gabrielchagasn@gmail.com>
2025-07-29 11:57:46 +0000

    Lambda 1

---
+++ Lambda/markup/Produto.java
@@ -0,0 +1,28 @@
+import java.util.function.Supplier;
+import java.util.function.Consumer;
+public class Produto {
+    private String nome;
+    private double preco;
+    private double percentualMarkup = 10;
+
+    public Produto(double preco, String nome){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Supplier<Double> precoComMarkup = () -> preco * (1 + percentualMarkup / 100);
+
+    public Consumer<Double> atualizarMarkup = novoMarkup -> this.percentualMarkup = novoMarkup;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public double getPercentualMarkup() {
+        return percentualMarkup;
+    }
+}
