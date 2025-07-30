64c4fe21d087dd2a59f4eb915cda48d92ada9d13
gabrielcn07 <gabrielchagasn@gmail.com>
2025-07-30 13:33:40 +0000

    Lambda fixacao 1

---
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ConsultaProdutos {
+    public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterioFiltro){
+        List<Produto> produtosFiltrados = new ArrayList<>();
+        for(Produto produto : produtos){
+            if(criterioFiltro.testar(produto)){
+                produtosFiltrados.add(produto);
+            }
+        }
+        return produtosFiltrados;
+    }
+}
