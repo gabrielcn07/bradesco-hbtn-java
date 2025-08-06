0ff8234fc10568120ddba9a8ab243ff6aaa99ac0
gabrielcn07 <gabrielchagasn@gmail.com>
2025-08-06 18:01:18 +0000

    Streams, Collectors e Optional 4

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
@@ -0,0 +1,47 @@
+import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido){
+        List<Produto> livrosList = new ArrayList<>();
+        for(Produto produto : pedido.getProdutos()){
+            if(produto.getCategoria() == CategoriaProduto.LIVRO){
+                livrosList.add(produto);
+            }
+        }
+        return livrosList;
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos){
+        return produtos.stream()
+        .sorted(Comparator.comparing(Produto::getPreco).reversed())
+        .findFirst()
+        .orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        return produtos.stream()
+            .filter(p -> p.getPreco() >= precoMinimo)
+            .collect(Collectors.toList());
+    }
+
+   public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos.stream()
+            .filter(pedido -> pedido.getProdutos().stream()
+                .anyMatch(produto -> produto.getCategoria() == CategoriaProduto.ELETRONICO))
+            .collect(Collectors.toList());
+    }
+
+   public static List<Produto> aplicar15PorcentoDescontoEletronicos(List<Produto> produtos) {
+        return produtos.stream()
+            .map(produto -> {
+                if (produto.getCategoria() == CategoriaProduto.ELETRONICO) {
+                    produto.setPreco(produto.getPreco() * 0.85); // aplica 15% de desconto
+                }
+                return produto;
+            })
+            .collect(Collectors.toList());
+    }
+
+}
