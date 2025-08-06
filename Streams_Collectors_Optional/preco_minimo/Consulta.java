4bf59eb1aace95f7eaf6f8788d7cb14baa31b34a
gabrielcn07 <gabrielchagasn@gmail.com>
2025-08-06 17:47:10 +0000

    Streams, Collectors e Optional 2

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,28 @@
+import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido){
+        List<Produto> livrosList = new ArrayList<>();
+        for(Produto produto : pedido.getProdutos()){
+            if(produto.getCategoria() == CategoriaProduto.LIVRO){
+                livrosList.add(produto);
+            }
+        }
+        return livrosList;
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos){
+        return produtos.stream()
+        .sorted(Comparator.comparing(Produto::getPreco).reversed())
+        .findFirst()
+        .orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        return produtos.stream()
+            .filter(p -> p.getPreco() >= precoMinimo)
+            .collect(Collectors.toList());
+    }
+}
