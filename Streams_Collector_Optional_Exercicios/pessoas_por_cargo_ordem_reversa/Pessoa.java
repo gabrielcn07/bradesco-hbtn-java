2818ce3692e64cb39b24e2444626eeebe34ea5da
gabrielcn07 <gabrielchagasn@gmail.com>
2025-08-11 13:00:35 +0000

    Streams, Collectors e Optional 0

---
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -0,0 +1,44 @@
+import java.util.Objects;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %.6f", codigo, nome, cargo, idade, salario).replace('.', ',');
+    }
+
+    @Override
+    public int compareTo(Pessoa outra) {
+        return Integer.compare(this.codigo, outra.codigo);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa)) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return codigo == pessoa.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
